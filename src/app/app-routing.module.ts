4114446baf8553d2342c3ee69435cb06000eac22
Matus Jurika <matus.jurika@zymestic.sk>
2020-08-18 17:29:53 +0200

    Základný skelet komponentov

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent
+	},
+	{
+		path: 'contact',
+		component: ContactComponent
 	}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material.module';
 import { MenuComponent } from './menu/menu.component';
-import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-	declarations: [AppComponent, MenuComponent, HomeComponent],
+	declarations: [AppComponent, MenuComponent, HomeComponent, ProjectsComponent, FooterComponent, ContactComponent, AboutComponent],
 	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
 	providers: [],
 	bootstrap: [AppComponent]
