6aef239a9d3049c05e0bb8d2e62dc1d777a5bb0c
Matus Jurika <matus.jurika@zymestic.sk>
2020-08-18 16:49:30 +0200

    Remake home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -10,8 +9,14 @@ const routes: Routes = [
 	}
 ];
 
+const routerOptions: ExtraOptions = {
+	anchorScrolling: 'enabled',
+	onSameUrlNavigation: 'reload',
+	scrollPositionRestoration: 'enabled'
+};
+
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, routerOptions)],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
